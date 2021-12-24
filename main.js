29c8134b6b972522ecc1747a6de130bbea2bfd9a
iamnaf <nafsulaiman@gmail.com>
2021-12-24 19:19:47 +0100

    modify the submit button, include functionality to check input value range in main.js and add max value in index.html

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+// variable declarations to interact with the HTML document
+
 const container = document.querySelector('.container');
+const cont =document.getElementById('cont');
 const color = document.querySelector('.color');
 const resetButton = document.querySelector('.button');
 const submitButton = document.querySelector('.submit');
@@ -11,26 +14,50 @@ resetButton.addEventListener('click', function reset() {
     document.getElementById('col').value = '';
 });
 
-// create grid
-submitButton.addEventListener('click', e => {
-    e.preventDefault();
-        let row = document.getElementById('row').value;
+
+/* The makeGrid() function is used to create empty grid cells on the screen 
+   based on the user input of height and width.
+   And it also checks for the minimum and maximum value of the values 
+   inputed.
+ */
+
+function makeGrid(){
+    let row = document.getElementById('row').value;
         let col = document.getElementById('col').value;
-        if (row < 0){
+        if (row < 0 && row){
             row = -1 * row;
         }
         if (col < 0){
             col = -1 * col;
         }
-        let size = row * col;
-        container.style.setProperty('--size', row)
-        for (let i = 0; i < size; i++) {
-            const div = document.createElement('div');
-            div.classList.add('pixel');
-            container.appendChild(div); 
+        if (col >= 1 && col <= 50 && row >= 1 && row <= 50){
+            let size = row * col;
+            container.style.setProperty('--size', row)
+            for (let i = 0; i < size; i++) {
+                const div = document.createElement('div');
+                div.classList.add('pixel');
+                container.appendChild(div); 
+            }
         }
+        else{
+            alert('Please check the input and ensure it is within range');
+        }
+}
+
+// The submit button event listener used to handle clicks on the submit button
+submitButton.addEventListener('click', e => {
+    e.preventDefault();
+    if (cont.innerHTML === ''){
+        makeGrid();
+    }
+    else{
+        cont.innerHTML = '';
+        makeGrid();
+    }
 });
 
+
+// the clicks event listener to know which cell has been clicked on to be painted
 container.addEventListener('mouseover', e => {
     pixels = document.querySelectorAll('.pixel');
     pixels.forEach(pixel => {
